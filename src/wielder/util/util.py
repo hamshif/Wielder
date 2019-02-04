cbc71bd2a3f7fe519b4dc8a0d6232f07d1a02902
Gideon Bar <gbar@marketo.com>
2019-02-04 02:29:35 +0200

    dir context change util

--- src/wielder/util/util.py
+++ src/wielder/util/util.py
@@ -1,3 +1,21 @@
+import os
+
+
+class DirContext:
+    """
+    Written by Ido Goodis
+    Context manager for changing the current working directory
+    """
+
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
 
 
 def replace_last(full, sub, rep=''):
