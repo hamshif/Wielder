a6d5050046d8f4b691e7e657f8703e9ee4e79504
Gideon Bar <gbar@marketo.com>
2019-06-16 08:49:28 +0300

    using object variables

--- src/wielder/wield/wield_service.py
+++ src/wielder/wield/wield_service.py
@@ -3,29 +3,45 @@ from wielder.wield.enumerator import PlanType
 
 from wielder.wield.modality import WieldMode
 from wielder.wield.planner import WieldPlan
+from wield_services.wield.deploy.util import get_conf
 
 
 class WieldService:
     """
     A class wrapping configuration and code to deploy a service on Kubernetes
-    disambiguation: service => a set of kubernetes resources e.g. deployment service storage ..
+    disambiguation: service => a set of kubernetes resources
+    e.g. micro-service comprised of deployment service storage ..
     By default it assumes a specific project structure
     It assumes specific fields in the configuration
     """
 
-    def __init__(self, name, module_root, mode=WieldMode(), conf_dir=None, plan_dir=None, plan_format=PlanType.YAML):
+    def __init__(self, name, module_root, project_override=False, mode=None, conf_dir=None,
+                 plan_dir=None, plan_format=PlanType.YAML):
 
         self.name = name
         self.module_root = module_root
-        self.mode = mode
+        self.mode = mode if mode else WieldMode()
         self.conf_dir = conf_dir if conf_dir else f'{module_root}conf'
         self.plan_dir = plan_dir if plan_dir else f'{module_root}plan'
 
-        self.wield_path = f'{self.conf_dir}/{mode.runtime_env}/{name}-wield.conf'
+        self.wield_path = f'{self.conf_dir}/{self.mode.runtime_env}/{name}-wield.conf'
 
         self.pretty()
 
-        self.conf = Cf.parse_file(self.wield_path)
+        if project_override:
+
+            print(f'\nOverriding module conf with project conf\n')
+
+            self.conf = get_conf(
+                runtime_env=self.mode.runtime_env,
+                deploy_env=self.mode.deploy_env,
+                module_paths=[self.wield_path]
+            )
+
+        else:
+            self.conf = Cf.parse_file(self.wield_path)
+
+        print('break')
 
         self.plan = WieldPlan(
             name=self.name,
