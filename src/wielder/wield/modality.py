39ce4475c3e55a1f2a42d9ab79138ae802669441
Gideon Bar <gbar@marketo.com>
2019-06-19 23:19:28 +0300

    added debug mode to madality

--- src/wielder/wield/modality.py
+++ src/wielder/wield/modality.py
@@ -2,12 +2,17 @@
 
 class WieldMode:
     """
-   WieldMode is used for polymorphism of image packing, provisioning, deployment ...
-   For example running on docker for development VS. running on GKE for quality engineering.
-   """
+    WieldMode is used for polymorphism of image packing, provisioning, deployment ...
+    For example running on docker for development VS. running on GKE for quality engineering.
+    """
 
-    def __init__(self, runtime_env='docker', deploy_env='dev'):
+    def __init__(self, runtime_env='docker', deploy_env='dev', debug_mode=False):
+        """
 
+        :param runtime_env: kubernetes on docker, minikube, gc, aws, azure ....
+        :param deploy_env: dev, int qa, prod ...
+        :param debug_mode: weather to enable remote debugging by allocating a port env variables ...
+        """
         self.runtime_env = runtime_env
         self.deploy_env = deploy_env
-
+        self.debug_mode = debug_mode
