04e81320d158f0501479a9e278372f0ca5f5defd
Gideon Bar <gbar@marketo.com>
2019-06-23 10:02:53 +0300

    added functionality for copying executable dir from local env to Dockerfile context for the purpose of packing images

--- src/wielder/util/imager.py
+++ src/wielder/util/imager.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-from shutil import copyfile
+from shutil import rmtree, copyfile, copytree
 from wielder.util.commander import async_cmd
 
 
@@ -19,7 +19,7 @@ def replace_file(origin_path, origin_regex, destination_path, final_name):
 
     target_file = async_cmd(f"find {origin_path} -name {origin_regex}")[0][:-1]
 
-    print(f"zip is: {target_file}")
+    print(f'Regex "{origin_regex}"" is fount in origin_path:  {target_file}')
 
     if target_file == '':
 
@@ -36,7 +36,52 @@ def replace_file(origin_path, origin_regex, destination_path, final_name):
     except Exception as e:
         print(str(e))
 
-    copyfile(target_file, full_destination)
+    copytree(target_file, full_destination)
+
+    print(f"successfully replaced {full_destination}")
+
+
+def replace_dir_contents(origin_path, origin_regex, destination_path, destination_dir_name='artifacts'):
+    """
+    Used to update executable code for docker image packing
+    e.g. interpreted code and artifacts.
+    Validates existence of directory.
+    Validates existence of a file regex in the directory for sanity purposes.
+    Removes old destination.
+    Copies directory contents to destination under artifacts directory
+
+    :param origin_path: path to directory containing executables to be packed into image
+    :param origin_regex: Origin file regex e.g. App*.zip (AppV1.zip, AppV2.zip ...)
+           to be found in path for sanity check
+    :param destination_path: A destination directory
+           where an the content of the executable directory can be copied to
+    :param destination_dir_name: the name of the destination directory defaults to: artifacts
+    :return:
+    """
+
+    target_file = async_cmd(f"find {origin_path} -name {origin_regex}")[0][:-1]
+
+    print(
+        f'Search results for regex <{origin_regex}> in origin_path:  {origin_path}:\n'
+        f'{target_file}'
+    )
+
+    if target_file == '':
+
+        print(f"couldn't find {origin_path}/{origin_regex}\nPlease make sure it exists in path")
+        return
+
+    print(f"Found {target_file} in target")
+
+    full_destination = f"{destination_path}/{destination_dir_name}"
+
+    try:
+        rmtree(full_destination)
+
+    except Exception as e:
+        print(str(e))
+
+    copytree(origin_path, full_destination)
 
     print(f"successfully replaced {full_destination}")
 
