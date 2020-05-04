45f37939533523637ca4502dbb6a501131f11f35
Gideon Bar <hamshif@gmail.com>
2020-05-04 13:21:19 +0300

    minor cleanup

--- src/setup.py
+++ src/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(name='wielder',
       version='0.1',
-      description='A reactive debuggable CICD & orchestration management tool'
+      description='A reactive debuggable CI-CD & orchestration management tool'
                   ' for local & cloud deployments e.g. kubernetes, airflow & data lakes',
       url='https://github.com/hamshif/Wielder.git',
       author='gbar',
       author_email='hamshif@gmail.com',
-      license='MIT',
+      license='Apache License Version 2.0',
       packages=find_packages(),
       zip_safe=False,
-      install_requires=['kubernetes', 'jprops']
+      install_requires=['kubernetes', 'rx', 'jprops']
       )
