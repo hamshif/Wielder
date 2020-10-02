3894272c7a72cec57ceb0099f71dd34ab79b987a
pepgid <gideon.bar@pepticom.com>
2020-10-02 12:11:19 +0300

    improved pyenv creation automation script

--- useful/wielder_pyenv.sh
+++ useful/wielder_pyenv.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
-
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "$0 is running from: $DIR"
 
 # make this file's location working dir
 cd "$(dirname "$0")"
 
+v=$1
 
-v='dud'
+if [ -z "$v" ]
+then
+      echo "\$v is empty"
+      v=wielder
+else
+      echo "\$v is NOT empty"
+fi
+
+echo "Creating or updating $v pyenv"
+
+python -V
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv deactivate
+
 existing_envs=$(pyenv virtualenvs)
 
 if [[ "$existing_envs" == *"$v"* ]]; then
@@ -18,16 +33,16 @@ else
 
 #    pyenv install 3.7.5
 
-    echo "$v virtualenv doesn't exist."
+    echo "$v virtualenv doesn't exist.";
     pyenv virtualenv 3.7.5 $v
 
-    echo activate dud and restart
+    pyenv activate $v
     echo 'pyenv activate $v'
     exit 0
 fi
 
 pip install --upgrade pip
 
-pip install gitpython rx Cython pyhocon flask confluent-kafka kafka-python Kazoo cassandra-driver kubernetes kafka
-
-pip install apache-airflow
+pip install --upgrade gitpython rx Cython pyhocon flask confluent-kafka kafka-python Kazoo cassandra-driver kubernetes kafka
+pip install --upgrade boto3
+pip install --upgrade apache-airflow
