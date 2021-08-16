047c2aa2594d9865533aff88bd9d47c3c46a9158
pepgid <gideon.bar@pepticom.com>
2021-08-16 17:56:45 +0300

    moved dirty testing for use of terraform credentials to test cleaner but not perfect

--- wielder/util/terraform_credential_helper.py
+++ wielder/util/terraform_credential_helper.py
@@ -4,11 +4,6 @@ import logging
 import os
 from os.path import expanduser
 
-# from wield_services.wield.deploy.configurer import get_project_deploy_mode
-from wield_services.wield.log_util import setup_logging
-
-from wielder.util.util import DirContext
-
 
 def as_export_cmd(prop_dict):
 
@@ -82,33 +77,10 @@ def get_aws_mfa_cred_command(role_name):
 
     if not as_string_cmd:
 
-        print("couldn't find credentials returning empty string")
+        logging.warning("couldn't find credentials returning empty string")
 
     return as_string_cmd
 
-
-if __name__ == "__main__":
-
-    setup_logging(log_level=logging.DEBUG)
-    # wield_mode, conf, action = get_project_deploy_mode()
-    #
-    # # import boto3
-    # # s3client = boto3.client('s3')
-    # # response = s3client.list_buckets()['Buckets']
-    #
-    # cred_string = get_aws_mfa_cred_command(conf.terraformer.super_cluster.cred_role)
-    #
-    # # print(cred_string)
-    #
-    # _home = expanduser("~")
-    # print(_home)
-    # dir_path = f"{_home}/stam/pep_eks"
-    #
-    # with DirContext(_home):
-    #
-    #     cmd = f'{cred_string}\nterraform plan'
-    #
-    #     print(cmd)
 
 
 
