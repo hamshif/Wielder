a03441d5808e103eabf2d8b68cbd5eb406007cd5
pepgid <gideon.bar@pepticom.com>
2021-11-05 11:33:37 +0200

    development utility for synchronizing local code with pods

---
+++ wielder/util/dev_util.py
@@ -0,0 +1,65 @@
+import logging
+
+from wield_services.wield.deploy.configurer import get_project_deploy_mode
+from wield_services.wield.deploy.util import get_project_locale
+from wielder.util.util import copy_file_to_pods
+from wielder.wield.deployer import get_pods
+
+
+def sync_dev_to_kube(locale, conf):
+    """
+    Synchronises files (code & configuration) from a development workstation and kubernetes pods
+    for development purposes
+
+    # Example of config need to be add to developer.conf
+    dev: {
+
+        push: true
+        dev_mode: false # if in dev mode then copy files to pod
+        pod_names: [airflow-worker] # list of pods in which we need to push modules
+        context: kind-pepticom-local
+
+        airflow-worker {
+               mount_folders: false # do we need mount pvc and clasess to airflow-worker (custom parameter)
+               module_list: [dud, pep-services, Wielder, pep-terraform]
+               namespace: airflow
+               pod_destination: /tmp/duds
+          }
+
+          bot {
+              module_list: [dud, pep-services, Wielder, pep-terraform]
+              namespace: bot
+              pod_destination: /bot_folder/super_bot_folder
+          }
+    }
+    :param locale:
+    :param conf:
+    :return:
+    """
+
+    if conf.dev.push:
+
+        for pod_search_name in conf.dev.pod_names:
+
+            pod_settings = conf.dev[pod_search_name]
+            pods = get_pods(pod_search_name, None, False, 'airflow')
+
+            for module_name in pod_settings.module_list:
+                copy_file_to_pods(
+                    pods=pods,
+                    src=f'{locale.super_project_root}/{module_name}',
+                    pod_dest=pod_settings.pod_destination,
+                    namespace=pod_settings.namespace,
+                    context=conf.dev.context
+                )
+
+
+if __name__ == "__main__":
+
+    _locale = get_project_locale(__file__)
+
+    _, _conf, _ = get_project_deploy_mode()
+
+    sync_dev_to_kube(_locale, _conf)
+
+
