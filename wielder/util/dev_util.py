6f8eff121f1d7bb710556987576f4e2bb35b966a
pepgid <gideon.bar@pepticom.com>
2021-11-16 21:03:24 +0200

    developer just chooses what to sync into pods for dev by developer.conf

--- wielder/util/dev_util.py
+++ wielder/util/dev_util.py
@@ -33,57 +33,63 @@ def sync_filtered_to_kube(conf):
     shutil.rmtree(stage)
     os.makedirs(stage, exist_ok=True)
 
+    sync_list = conf.dev_sync.sync_list
+
     for sync, sync_conf in conf.dev_sync.sync_dirs.items():
 
-        sync_dir = sync_conf.name
-        src = sync_conf.src
+        if sync in sync_list:
 
-        fd = conf.ignored_dirs
-        ff = conf.ignored_files
+            sync_dir = sync_conf.name
+            src = sync_conf.src
 
-        gen = filter_walk(
-            src,
-            f_filter_dirs=is_valid_dir,
-            forbidden_dirs=fd,
-            f_filter_files=is_valid_file,
-            forbidden_files=ff
-        )
+            fd = conf.ignored_dirs
+            ff = conf.ignored_files
 
-        nd = None
-
-        for dir_path, sub_dirs, file_names in gen:
-
-            nd = dir_path.replace(src, '')
-
-            stage_dest = f'{stage}/{sync_dir}/{nd}'
-            os.makedirs(stage_dest, exist_ok=True)
-
-            for file_name in file_names:
-
-                src_file = f'{dir_path}/{file_name}'
-                tmp_file = f'{stage_dest}/{file_name}'
-
-                shutil.copyfile(src_file, tmp_file)
-
-            print(f'dir_path: {nd}')
-            print(f'sub_dir_names: {sub_dirs}')
-            print(f'file_names: {file_names}')
-
-        if nd is not None:
-
-            pod_search_name = sync_conf.pod_search_name
-            pods = get_pods(pod_search_name, conf.kube_context, False, sync_conf.namespace)
-
-            src = f'{stage}/{sync_dir}'
-            dst = sync_conf.dst
-
-            copy_file_to_pods(
-                pods=pods,
-                src=src,
-                pod_dest=dst,
-                namespace=sync_conf.namespace,
-                context=conf.kube_context
+            gen = filter_walk(
+                src,
+                f_filter_dirs=is_valid_dir,
+                forbidden_dirs=fd,
+                f_filter_files=is_valid_file,
+                forbidden_files=ff
             )
+
+            nd = None
+
+            for dir_path, sub_dirs, file_names in gen:
+
+                nd = dir_path.replace(src, '')
+
+                stage_dest = f'{stage}/{sync_dir}/{nd}'
+                os.makedirs(stage_dest, exist_ok=True)
+
+                for file_name in file_names:
+
+                    src_file = f'{dir_path}/{file_name}'
+                    tmp_file = f'{stage_dest}/{file_name}'
+
+                    shutil.copyfile(src_file, tmp_file)
+
+                print(f'dir_path: {nd}')
+                print(f'sub_dir_names: {sub_dirs}')
+                print(f'file_names: {file_names}')
+
+            if nd is not None:
+
+                pod_search_name = sync_conf.pod_search_name
+                pods = get_pods(pod_search_name, conf.kube_context, False, sync_conf.namespace)
+
+                src = f'{stage}/{sync_dir}'
+                dst = sync_conf.dst
+
+                copy_file_to_pods(
+                    pods=pods,
+                    src=src,
+                    pod_dest=dst,
+                    namespace=sync_conf.namespace,
+                    context=conf.kube_context
+                )
+        else:
+            logging.debug(f'{sync} wont be loaded')
 
 
 def sync_dev_to_kube(locale, conf):
