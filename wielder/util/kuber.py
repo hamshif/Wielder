0aeddf0d5a535eb08a7f781369c4da25b3c42129
pepgid <gideon.bar@pepticom.com>
2022-03-02 01:31:45 +0200

    extracted delete pvc functionality for use

--- wielder/util/kuber.py
+++ wielder/util/kuber.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+from wielder.wield.kube_probe import get_kube_namespace_resources_by_type
+
 
 def update_eks_context(runtime_env, cred_profile, region, kube_cluster_name):
 
@@ -16,3 +18,18 @@ def update_eks_context(runtime_env, cred_profile, region, kube_cluster_name):
         logging.warning("Gaining context for GCP hasn't been written")
     elif runtime_env == 'azure':
         logging.warning("Gaining context for Azure hasn't been written")
+
+
+def delete_pvcs(context, namespace, partial_name):
+
+    pvcs = get_kube_namespace_resources_by_type(context, namespace, 'pvc')
+
+    for pvc in pvcs['items']:
+
+        pvc_name = pvc['metadata']['name']
+
+        if partial_name in pvc_name:
+
+            _cmd = f"kubectl --context {context} -n {namespace} delete pvc {pvc_name};"
+            logging.info(f'Running cmd\n{_cmd}')
+            os.system(_cmd)
