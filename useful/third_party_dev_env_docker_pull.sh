721aa05ee7d5e3aecb2dd2096918211af67fe4a6
pepdani <daniel.polevikov@pepticom.com>
2022-09-14 15:28:27 +0300

    Some of the docker pull quay commands are updated

--- useful/third_party_dev_env_docker_pull.sh
+++ useful/third_party_dev_env_docker_pull.sh
@@ -17,8 +17,8 @@ docker pull docker.io/datastax/cass-config-builder:1.0.4
 docker pull docker.io/busybox:1.35.0
 docker pull docker.io/k8ssandra/system-logger:6c64f9c4
 
-docker pull quay.io/prometheus/prometheus:v2.36.1                                                                      c70a5daad275   3 months ago        206MB
-docker pull quay.io/prometheus-operator/prometheus-config-reloader:v0.57.0                                                                      bc9b4082034e   3 months ago        12.2MB
-docker pull quay.io/prometheus-operator/prometheus-operator:v0.57.0                                                                      0a51339a8b4a   3 months ago        51.6MB
-docker pull quay.io/prometheus/alertmanager:v0.24.0                                                                      913727cc7dd5   5 months ago        59.8MB
+docker pull quay.io/prometheus/prometheus:v2.36.1
+docker pull quay.io/prometheus-operator/prometheus-config-reloader:v0.57.0
+docker pull quay.io/prometheus-operator/prometheus-operator:v0.57.0
+docker pull quay.io/prometheus/alertmanager:v0.24.0
 docker pull quay.io/prometheus/node-exporter:v1.3.1
