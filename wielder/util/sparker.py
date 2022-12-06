2b90c068b4ad7788d5994019a939081e10578eb3
Gideon Bar <gideon.bar@pepticom.com>
2022-12-06 12:55:39 +0200

    Quick plaster for enabling chaining of local spark jobs

--- wielder/util/sparker.py
+++ wielder/util/sparker.py
@@ -284,7 +284,9 @@ class DevSparker(Sparker):
     """
 
     def is_job_active(self, jobs_path=['jobs']):
-        raise Exception("is_job_active function must be implemented in DevSparker class")
+
+        return False
+        # raise Exception("is_job_active function must be implemented in DevSparker class")
 
     def __init__(self, conf, launch_env=RuntimeEnv.MAC):
 
