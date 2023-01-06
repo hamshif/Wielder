07988451ccaafbcbec54798b42d35ebee873adae
Gideon Bar <gideon.bar@pepticom.com>
2023-01-06 14:03:23 +0200

    installing Cassandra driver just to make sure about the dependencies first time

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
         'Cython==0.29.32', 'GitPython==3.1.30', 'PyYAML==6.0', 'kubernetes==25.3.0', 'rx==3.2.0', 'jprops==2.0.2',
         'pyhocon==0.3.59', 'requests==2.28.1', 'deepdiff==6.2.1', 'botocore==1.29.44', 'boto3==1.26.44',
         'kazoo==2.9.0', 'kafka==1.3.5', 'confluent-kafka',
-        # 'cassandra-driver==3.25.0', TODO use the next version supporting up to Python 3.11
+        'cassandra-driver',
+        # TODO use the next version of cassandra driver supporting up to Python 3.11
     ],
     keywords=['CI-CD', 'Kubernetes', 'Reactive'],
     classifiers=[
