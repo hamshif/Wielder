1651b32f063e558d3b0ef2916da02f6eeb28d6c9
gideon <hamshif@gmail.com>
2025-02-16 21:35:08 +0200

    WSL instructions and bash

---
+++ useful/wsl/WSL.md
@@ -0,0 +1,74 @@
+
+
+# Install WSL
+=
+1. Open PowerShell as an Administrator.
+1. Run the following command to install WSL:
+1. powershell
+```
+wsl --install
+```
+
+1. Restart your computer if prompted.
+1. Set up your Linux distribution by following the on-screen instructions.
+
+Install Docker
+==
+
+1. Download Docker Desktop for Windows from the Docker website.
+
+1. Run the installer and follow the setup instructions.
+
+1. Make sure to configure Docker to use the WSL 2 backend:
+
+1. Open Docker Desktop settings.
+
+1. Go to the "General" tab and enable "Use the WSL 2 based engine."
+
+1. Apply & Restart Docker Desktop.
+
+1. Install Kubernetes On Docker
+1. Open Docker Desktop settings.
+
+1. Go to the "Kubernetes" tab.
+
+1. Check "Enable Kubernetes" and click "Apply & Restart."
+
+1. Wait for Kubernetes to start.
+
+Install Sublime
+=
+
+1. Download Sublime Text from the Sublime Text website.
+
+1. Run the installer and follow the setup instructions.
+
+1. Point to WSL Directory
+1. Open Sublime Text.
+
+1. Go to "Preferences" > "Settings".
+
+1. Add the following lines to your settings file to point to your WSL directory:
+
+```
+"folders":
+[
+    {
+        "path": "//wsl$/Ubuntu/home/your_username"
+    }
+]
+
+```
+
+Install SourceTree
+=
+
+Download SourceTree from the SourceTree website.
+
+Run the installer and follow the setup instructions.
+
+Log in with your Atlassian account or create a new one.
+
+Connect to your Bitbucket or GitHub account if needed.
+
+Follow the prompts to complete the setup.
---
+++ useful/wsl/prepare_wsl_ubuntu.sh
@@ -0,0 +1,162 @@
+#!/usr/bin/env bash
+
+sudo apt update -y && sudo apt upgrade -y
+
+# Git Large File Storage
+sudo apt install -y git-lfs
+
+# OpenSSL
+sudo apt install -y openssl
+
+# librdkafka
+sudo apt install -y librdkafka-dev
+
+# Zsh
+sudo apt install -y zsh
+
+# Wget
+sudo apt install -y wget
+
+# OpenJDK 11 and 8
+sudo apt install -y openjdk-11-jdk openjdk-8-jdk
+
+# Maven
+sudo apt install -y maven
+
+# Scala
+sudo apt install -y scala
+
+# Kubectl
+sudo apt install -y kubectl
+
+# Helm
+sudo apt install -y helm
+
+# AWS CLI
+sudo apt install -y awscli
+
+
+# git credentials for
+if [[ $(command ssh-add -l | grep  `ssh-keygen -lf ~/.ssh/id_rsa  | awk '{print $2}'`) == "" ]]; then
+    echo "Adding key to  ~/.ssh/id_rsa"
+    ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+    # Follow the prompts to save the key (usually in ~/.ssh/id_rsa)
+
+else
+    echo "Key ~/.ssh/id_rsa already exists"
+fi
+
+
+curl https://pyenv.run | bash
+
+# Add pyenv to bash so that it loads automatically
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
+# Restart shell
+exec "$SHELL"
+
+pyenv install 3.10
+pyenv virtualenv 3.10 wielder
+
+# Install jenv
+
+git clone https://github.com/jenv/jenv.git ~/.jenv
+#echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.zshrc
+#echo 'eval "$(jenv init -)"' >> ~/.zshrc
+eval "$(cat ~/.zshrc | tail -n +10)"
+jenv enable-plugin export
+eval "$(cat ~/.zshrc | tail -n +10)"
+jenv add /usr/lib/jvm/java-1.8.0-openjdk-amd64/
+jenv add /usr/lib/jvm/java-11-openjdk-amd64/
+
+
+if [[ ! $(command -v spark-submit) == "" ]]; then
+
+    printf "spark exists\n"
+
+    spark-shell --version
+
+    printf 'if you want the newest spark install with brew run:\n\n'
+
+    printf '  brew install apache-spark -vd\n'
+
+else
+
+  echo 'installing spark'
+  wget https://archive.apache.org/dist/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz -P ~/Downloads
+  mkdir -p ~/hadoop/spark-3.3.1
+  tar -xvzf ~/Downloads/spark-3.3.1-bin-hadoop3.tgz -C ~/hadoop/spark-3.3.1 --strip 1
+  rm -f ~/Downloads/spark-3.3.1-bin-hadoop3.tgz
+  echo 'export SPARK_HOME=~/hadoop/spark-3.3.1' >> ~/.zshrc
+  echo 'export PATH=$SPARK_HOME/bin:$PATH' >> ~/.zshrc
+  echo 'export PYSPARK_PYTHON=/Users/$HOME/.pyenv/shims/python3' >> ~/.zshrc
+  echo 'installed spark'
+
+fi
+
+# Install ansible via pip
+#pip install ansible==2.9.6
+
+
+
+# Activate pyenv and install all packages
+v=$1
+if [ -z "$v" ]
+then
+      echo "\$v is empty"
+      v=wielder
+else
+      echo "\$v is NOT empty"
+fi
+
+pyenv activate $v
+
+../../package_py.bash
+
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+echo "execute <EDITOR=nano kubectl edit deployment metrics-server -n kube-system>"
+echo "add the following to the args: - --kubelet-insecure-tls"
+
+# Enable completion
+source <(kubectl completion zsh)
+# Add to zshrc for persistence
+echo "source <(kubectl completion zsh)" >> ~/.zshrc
+
+# Enable completion
+source <(helm completion zsh)
+
+# Add to zshrc for persistence
+echo "source <(helm completion zsh)" >> ~/.zshrc
+
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable
+helm repo add k8ssandra https://helm.k8ssandra.io/stable
+helm repo add kminion https://raw.githubusercontent.com/cloudhut/kminion/master/charts/archives
+helm repo add elastic https://helm.elastic.co
+
+helm repo update
+
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+# Add tfenv to bash so that it loads automatically
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >>
+
+# Restart shell
+exec "$SHELL"
+
+# Install the latest Terraform version
+#tfenv install latest
+
+# Set the latest version as the global version
+#tfenv use latest
+
+tfenv install 1.1.9
+tfenv use 1.1.9
+terraform -install-autocomplete
+
+# Restart shell
+exec "$SHELL"
