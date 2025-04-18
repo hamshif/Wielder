2ec187745365313772730c44c68013c3f3d0697c
gideon <hamshif@gmail.com>
2025-04-18 07:31:21 +0300

    Wielder uses new 3.12 python and cassandra driver

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-_version = '0.5.0'
+_version = '0.6.0'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -24,7 +24,6 @@ setup(
         'pyhocon', 'requests', 'deepdiff', 'botocore', 'boto3', 'tabulate',
         'kazoo', 'kafka', 'confluent-kafka', 'google-api-python-client', 'google-auth-oauthlib',
         'cassandra-driver', 'tqdm'
-        # TODO use the next version of cassandra driver supporting up to Python 3.11
     ],
     keywords=['CI-CD', 'Kubernetes', 'Reactive'],
     classifiers=[
@@ -34,5 +33,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
