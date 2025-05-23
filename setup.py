c7e2a557964451c3d4e34f57d0978f3865b7a1d1
gideon <hamshif@gmail.com>
2025-05-23 21:35:34 +0300

    added cool docker lib

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'wheel', 'Cython', 'GitPython', 'PyYAML', 'kubernetes', 'rx==3.2.0', 'jprops',
         'pyhocon', 'requests', 'deepdiff', 'botocore', 'boto3', 'tabulate',
         'kazoo', 'kafka', 'confluent-kafka', 'google-api-python-client', 'google-auth-oauthlib',
-        'cassandra-driver', 'tqdm'
+        'cassandra-driver', 'tqdm', 'docker'
     ],
     keywords=['CI-CD', 'Kubernetes', 'Reactive'],
     classifiers=[
